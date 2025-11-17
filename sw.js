6c3d5421fe94a9a2d22ad475e9f79fdd290f7c37
hiranotakuto1996-dot <hirano.takuto1996@gmail.com>
2025-11-17 12:59:29 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'geofine-alarm-v1';
+const urlsToCache = [
+  '/my-pwa-files/',            // ホームページ
+  '/my-pwa-files/index.html',  // Base44 出力の HTML
+  '/my-pwa-files/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) return caches.delete(name);
+        })
+      )
+    )
+  );
+});
